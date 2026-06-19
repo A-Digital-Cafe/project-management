d7e439b06863baf5fbddfd701e4285c5dafc98a7
Abbytec <gpsmurfs@gmail.com>
2026-06-18 21:49:21 -0300

    feat: implement async bootstrap function with maintenance check

--- apps/adc-project-manager/src/main.tsx
+++ apps/adc-project-manager/src/main.tsx
@@ -2,16 +2,23 @@ import "@ui-library/utils/react-jsx";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { redirectIfUnderMaintenance } from "@common/utils/module-availability.js";
 import "@ui-library";
 import "@ui-library/styles";
 import "./styles/tailwind.css";
 
-const container = document.getElementById("root");
-if (container) {
-	const root = createRoot(container);
-	root.render(
-		<React.StrictMode>
-			<App />
-		</React.StrictMode>
-	);
+async function bootstrap() {
+	if (await redirectIfUnderMaintenance("adc-project-manager")) return;
+
+	const container = document.getElementById("root");
+	if (container) {
+		const root = createRoot(container);
+		root.render(
+			<React.StrictMode>
+				<App />
+			</React.StrictMode>
+		);
+	}
 }
+
+void bootstrap();
